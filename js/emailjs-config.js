5e8a87c72e00c24da6bb289a88922b0c79d7b245
scastrocid <scastro@agenciase.org>
2026-05-30 18:14:51 -0400

    correo y ocultar html

---
+++ js/emailjs-config.js
@@ -0,0 +1,9 @@
+window.EMAILJS_CONFIG = {
+  publicKey: "4uVMqxVu5ND5dpDfr",
+  serviceId: "service_tvw82wd",
+  templateId: "template_eot3ll6",
+};
+
+if (typeof emailjs !== "undefined") {
+  emailjs.init(window.EMAILJS_CONFIG.publicKey);
+}
